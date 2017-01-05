05982641be765c7ff9988d687feee6d28988e48a
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-05 11:36:01 -0600

    added new rando button

--- handles.js
+++ handles.js
@@ -1,3 +1,39 @@
+
+
+function newUser (data) {
+
+  //handlebars step one: grap the html from the script tag
+  let userHTML = $('#handleGrab').html()
+
+  //handlebars step two: compile it into a template
+  let userTemplate = Handlebars.compile(userHTML)
+
+  //handlebars step three:render the HTML by passing the data to the template
+  let userOutput = userTemplate(data)
+
+  console.log(userOutput)
+
+  //handlebars step four: put the complete HTML into the DOM
+  $('#displayUser').html(userOutput)
+
+}
+
+// event listener for new rando button
+$('#generate').click(function() {
+  getUser()
+   .then(function(data) {
+      return newUser(data)
+   })
+})
+
+
+
+
+
+
+
+
+
 //sample data modified from randomUser.me
 // var randomUser = {
 //   "results": [
@@ -22,22 +58,3 @@
 //     }
 //   ]
 // }
-
-
-function newUser (data) {
-
-  //handlebars step one: grap the html from the script tag
-  let userHTML = $('#handleGrab').html()
-
-  //handlebars step two: compile it into a template
-  let userTemplate = Handlebars.compile(userHTML)
-
-  //handlebars step three:render the HTML by passing the data to the template
-  let userOutput = userTemplate(data)
-
-  console.log(userOutput)
-
-  //handlebars step four: put the complete HTML into the DOM
-  $('#displayUser').append(userOutput)
-
-}
