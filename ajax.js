1d90ea3e25a1a2f58996ff9b77457ff2f0023184
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-05 11:25:47 -0600

    finished ajax call

--- ajax.js
+++ ajax.js
@@ -1,15 +1,17 @@
 function getUser() {
+   return new Promise( function( resolve, reject ) {
 
-   let promise = new Promise(resolve, reject) {
-
-          return $.ajax({
-           url: 'https://randomuser.me/api/',
-           dataType: 'json',
-           success: function(data) {
-             console.log(data);
-           }
-         });
-
-   }
-
+      $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+      })
+      .done(function(data) {
+         resolve(data);
+      })
+   })
 }
+
+getUser()
+   .then(function(data) {
+      return newUser(data)
+   })
--- handles.js
+++ handles.js
@@ -1,38 +1,43 @@
 //sample data modified from randomUser.me
-var randomUser = {
-  "results": [
-    {
-      "gender": "female",
-      "name": {
-        "title": "ms",
-        "first": "stella",
-        "last": "meyer"
-      },
-      "location": {
-        "street": "7385 kapellenweg",
-        "city": "schmalkalden-meiningen",
-        "state": "saarland",
-        "postcode": 58225
-      },
-      "email": "stella.meyer@example.com",
-      "picture": {
-        "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
-      },
-      "nat": "DE"
-    }
-  ]
+// var randomUser = {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "ms",
+//         "first": "stella",
+//         "last": "meyer"
+//       },
+//       "location": {
+//         "street": "7385 kapellenweg",
+//         "city": "schmalkalden-meiningen",
+//         "state": "saarland",
+//         "postcode": 58225
+//       },
+//       "email": "stella.meyer@example.com",
+//       "picture": {
+//         "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
+//       },
+//       "nat": "DE"
+//     }
+//   ]
+// }
+
+
+function newUser (data) {
+
+  //handlebars step one: grap the html from the script tag
+  let userHTML = $('#handleGrab').html()
+
+  //handlebars step two: compile it into a template
+  let userTemplate = Handlebars.compile(userHTML)
+
+  //handlebars step three:render the HTML by passing the data to the template
+  let userOutput = userTemplate(data)
+
+  console.log(userOutput)
+
+  //handlebars step four: put the complete HTML into the DOM
+  $('#displayUser').append(userOutput)
+
 }
-
-//handlebars step one: grap the html from the script tag
-let userHTML = $('#handleGrab').html()
-
-//handlebars step two: compile it into a template
-let userTemplate = Handlebars.compile(userHTML)
-
-//handlebars step three:render the HTML by passing the data to the template
-let userOutput = userTemplate(randomUser)
-
-console.log(userOutput)
-
-//handlebars step four: put the complete HTML into the DOM
-$('#displayUser').append(userOutput)
